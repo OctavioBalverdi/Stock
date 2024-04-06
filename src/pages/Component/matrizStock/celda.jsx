7e6d863b4b742dfb08c9983dbcbd84ab5d9294db
Rodrigo <rodrigoamor94@gmail.com>
2024-04-06 19:30:00 -0300

    Poner pestañas

---
+++ src/pages/Component/matrizStock/celda.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const Celda = ({ contenido }) => {
+  return <div>{contenido}
+  {console.log(contenido)}</div>;
+};
+
+export default Celda;
--- src/pages/domain/stock.jsx
+++ src/pages/domain/stock.jsx
@@ -1,16 +1,34 @@
 
 import TarjetaStock from "../Component/tarjetas/TarjetaStock";
-
+import Matriz from "../Component/matrizStock/matrizS";
 import "./Stockpag.css";
 
 export const Stock=()=>{
 
-    return(
+  const matrizEjemplo = [
+    // Estante 1
+    [
+      ['A', 'B', 'C'],
+      ['D', 'E', 'F'],
+    ],
+    // Estante 2
+    [
+      ['G', 'H', 'I'],
+      ['J', 'K', 'L'],
+    ],
+    // ... más estantes
+  ];
+
+  return <Matriz estantes={matrizEjemplo} />;
+  
+
+   /* return(
           <>
         <div className="TarjetaStock">
-            <TarjetaStock/>
+            {<TarjetaStock/> }
+
         </div>
           
           </>
-    )
+    )*/
 }
