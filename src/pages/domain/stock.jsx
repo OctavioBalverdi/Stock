32b2949f9357b02094dd3ae3792401328f479c30
Rodrigo Love <rodrigoamor94@gmail.com>
2024-04-07 21:25:13 -0300

    funcionalidad de elegir dimencion de matriz

--- src/pages/domain/stock.jsx
+++ src/pages/domain/stock.jsx
@@ -3,7 +3,7 @@ import TarjetaStock from "../Component/tarjetas/TarjetaStock";
 import Matriz from "../Component/matrizStock/matrizS";
 import "./Stockpag.css";
 
-export const Stock=()=>{
+/*export const Stock=()=>{
 
   const matrizEjemplo = [
     // Estante 1
@@ -22,7 +22,7 @@ export const Stock=()=>{
   return <Matriz estantes={matrizEjemplo} />;
   
 
-   /* return(
+    return(
           <>
         <div className="TarjetaStock">
             {<TarjetaStock/> }
@@ -30,5 +30,26 @@ export const Stock=()=>{
         </div>
           
           </>
-    )*/
+    )
 }
+*/
+export const Stock = () => {
+  // Define variables para configurar la cantidad de estantes, filas y columnas :*
+  const numEstantes = 4;
+  const numFilas = 4;
+  const numColumnas = 5;
+
+  // Crea una matriz dinámica basada en las variables :)
+  const matrizEjemplo = [];
+  for (let i = 0; i < numEstantes; i++) {
+    matrizEjemplo.push([]);
+    for (let j = 0; j < numFilas; j++) {
+      matrizEjemplo[i].push([]);
+      for (let k = 0; k < numColumnas; k++) {
+        matrizEjemplo[i][j].push(`${String.fromCharCode(65 + i)}${j + 1}${String.fromCharCode(65 + k)}`);
+      }
+    }
+  }
+
+  return <Matriz estantes={matrizEjemplo} />;
+};
